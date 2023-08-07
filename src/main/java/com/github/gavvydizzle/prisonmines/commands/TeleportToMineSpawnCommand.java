3f127f6731bc57e601eeb14e367bcd6485dd6f21
gjdim <gjdimpter@gmail.com>
2023-08-07 10:37:56 -0400

    Update mine teleport command

--- src/main/java/com/github/gavvydizzle/prisonmines/commands/TeleportToMineSpawnCommand.java
+++ src/main/java/com/github/gavvydizzle/prisonmines/commands/TeleportToMineSpawnCommand.java
@@ -14,22 +14,46 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class TeleportToMineSpawnCommand implements TabExecutor {
 
     private final MineManager mineManager;
+    private final HashMap<UUID, String> lastVisitedMine;
 
     public TeleportToMineSpawnCommand(MineManager mineManager) {
         this.mineManager = mineManager;
+        lastVisitedMine = new HashMap<>();
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
 
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.YELLOW + "/mine <id>");
+            if (lastVisitedMine.containsKey(player.getUniqueId())) {
+                Mine mine = mineManager.getMine(lastVisitedMine.get(player.getUniqueId()));
+                if (mine == null) {
+                    sender.sendMessage(ChatColor.YELLOW + "The last mine you visited no longer exists");
+                    return true;
+                }
+                else {
+                    if (!mine.hasAccessPermission(player)) {
+                        sender.sendMessage(Messages.cannotAccessMine);
+                        return true;
+                    }
+
+                    if (!mine.teleportToSpawn((Player) sender)) {
+                        sender.sendMessage(Messages.noSpawnLocation.replace("{id}", mine.getId()));
+                    }
+                }
+            }
+            else {
+                sender.sendMessage(ChatColor.YELLOW + "/mine <id>");
+            }
             return true;
         }
 
@@ -39,7 +63,6 @@ public class TeleportToMineSpawnCommand implements TabExecutor {
             return true;
         }
 
-        Player player = (Player) sender;
         if (!mine.hasAccessPermission(player)) {
             sender.sendMessage(Messages.cannotAccessMine);
             return true;
@@ -47,6 +70,9 @@ public class TeleportToMineSpawnCommand implements TabExecutor {
 
         if (!mine.teleportToSpawn((Player) sender)) {
             sender.sendMessage(Messages.noSpawnLocation.replace("{id}", mine.getId()));
+        }
+        else {
+            lastVisitedMine.put(player.getUniqueId(), mine.getId());
         }
 
         return true;
